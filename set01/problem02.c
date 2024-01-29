da77ad96b4251d162adbe1c393c156f7a3f26d2d
Yutisha <eyutisha.bsc23@rvu.edu.in>
2024-01-29 12:01:16 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,12 @@
 //2. Write a C program to add two numbers.
-
+#include<stdio.h>
+int main()
+{
+ int a, b, sum;
+ printf("enter a\n");
+ scanf("%d",&a);
+ printf("enter b\n");
+ scanf("%d",&b);
+ sum=a+b;
+ return sum;
+}
