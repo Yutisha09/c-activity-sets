4c3bfe317329871d8fe7ef21c8862b47c2714ce3
Yutisha <eyutisha.bsc23@rvu.edu.in>
2024-01-31 11:18:29 +0530

    test

---
+++ set01/problem10.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void input_two_strings(char *str1, char *str2)
+{
+    printf("enter the 1st string\n");
+    scanf("%s",str1);
+    printf("enter the 2nd string\n");
+    scanf("%s",str2);
+}
+int stringcompare(char *str1, char *str2)
+{
+    int i;
+    for (i=0; str1[i]==str2[i]; str1[i]!='\0' && str2[i]!='\0')
+    if (str1[i]>str2[i])
+    {
+        return 0;
+    }
+     else if (str2[i]>str1[i])
+    {
+        return 1;
+    }
+    else return 2;
+}
+
+void output(char *str1, char *str2, int result)
+{
+    if(result=0)
+    {
+        printf("%s is greater than %s",str1,str2);
+    }
+     else if(result=1)
+    {
+        printf("%s is greater than %s",str2,str1);
+    }
+    else
+    {
+        printf("%s is equall to %s",str1,str2);
+    }
+}
+int main()
+{
+    char str1[100];
+    char str2[100];
+    input_two_strings(&str1,&str2);
+    int result;
+    result=stringcompare(str1,str2);
+    output(str1,str2,result);
+    return 0;
+}
