1170283c19c52f033f5e4d22a1a6eed6c8b7c97d
Yutisha <eyutisha.bsc23@rvu.edu.in>
2024-02-24 23:31:50 +0530

    test

--- set03/problem04.c
+++ set03/problem04.c
@@ -1,4 +1,35 @@
 #include<stdio.h>
-int input();
-int find_fibo(int n);
-void output(int n, int fibo);
+int input()
+{
+    int n;
+    printf("enter the no.\n");
+    scanf("%d",&n);
+    return n;
+}
+int find_fibo(int n,int a[n])
+{
+    int i;
+    int j;
+    int fibo=0;
+    for ( i = 0;i<n;i++)
+    {   
+        fibo=a[i]++;
+
+    }
+    return fibo;
+    
+}
+void output(int n, int fibo)
+{
+    printf("the %d no. of the fibannaci series is %d",n,fibo);
+}
+int main()
+{
+    int n;
+    n=input();
+    int a[n];
+    int fibo=0;
+    fibo=find_fibo(n,a);
+    output(n,fibo);
+    return 0;
+}
